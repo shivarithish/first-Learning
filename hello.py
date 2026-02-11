840ee24b0db9f0e86124da3c2511df232916937b
Mathan <mathanmk6665@gmail.com>
2026-02-11 17:58:16 +0530

    mathan added few lines in  ur code

--- hello.py
+++ hello.py
@@ -2,7 +2,10 @@ print("hello is given by shiva")
 age=12
 if age>18:
     print("eligible for voting")
-elif age=18:
+elif age==18:
     print("apply for voter id")
+elif age<60:
+    print("update ur voter id")
+
 else:
-    print("not eligible for voter id")
+    print("not eligible for voting")
